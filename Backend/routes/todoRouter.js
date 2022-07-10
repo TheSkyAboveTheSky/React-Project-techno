20997d9bf0ea7ba71b660ffa14e0b2665d0977af
Adham ElGazoly <adham20191701021@cis.asu.edu.eg>
2022-07-11 00:32:55 +0200

    Add new route

--- Backend/routes/todoRouter.js
+++ Backend/routes/todoRouter.js
@@ -1,13 +1,15 @@
 const express = require ('express')
 const router = express.Router()
 var bodyParser = require('body-parser')
-const { getAllTasks , addNewTask} = require('../controller/todoController')
+const { getAllTasks , getAllTeams ,addNewTask  } = require('../controller/todoController')
 
 
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/all',getAllTasks)
+
+router.get('/teams',getAllTeams)
 
 router.post('/', urlencodedParser , addNewTask)
 
